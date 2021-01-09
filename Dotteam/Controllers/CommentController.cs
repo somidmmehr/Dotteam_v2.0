c0accc6420f7948a36c43cc280228408a90f0792
S.OMID M.MEHR <s.omid.m.mehr@gmail.com>
2021-01-09 20:33:43 +0330

    First commit

--- Dotteam/Controllers/CommentController.cs
+++ Dotteam/Controllers/CommentController.cs
@@ -69,7 +69,7 @@ namespace Dotteam.Controllers
             if (ModelState.IsValid)
             {
                 commentModel.CreatedAt = DateTime.Now;
-                commentModel.State = "Show";
+                commentModel.State = "Shows";
                 _context.Add(commentModel);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
